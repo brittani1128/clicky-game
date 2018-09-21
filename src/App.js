4959ccfa59a7d4066dfeb8dd1ad2a6ebcf9f8ee1
Brittani <brittani1128@gmail.com>
2018-09-20 22:44:18 -0500

    first commit, set up files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import CharacterCard from "./components/CharacterCard";
+// import characters from "./characters.json";
+
+const App = () => {
+return (
+  <div>
+    <CharacterCard />
+    <CharacterCard />
+    <CharacterCard />
+  </div>
+)
+}
+
+export default App;
---
+++ src/components/CharacterCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./CharacterCard.css";
+
+
+const CharacterCard = () => {
+    <div className="card">
+        <div className="img-container">
+            <img alt="pic" src="https://via.placeholder.com/150x150/"/>
+        </div>
+    </div>
+
+}
+
+export default CharacterCard;
