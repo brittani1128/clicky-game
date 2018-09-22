4c9bf70f651d6cd50595a03363147f1af054f6db
Brittani <brittani1128@gmail.com>
2018-09-22 17:40:02 -0500

    add cartoon images

--- src/components/Game/CharacterCard/CharacterCard.js
+++ src/components/Game/CharacterCard/CharacterCard.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./CharacterCard.css";
 
+const styles = {
+    image: {
+        height:150,
+        width:150
+    }
+}
 
 const CharacterCard = (props) => {
     return (
   
     <div className="card">
         <div className="img-container">
-            <img className="image" alt={props.name} alt="pic" src={props.image} onClick={() => props.handleClick(props.id)}/>
+            <img className="image" style={styles.image} alt={props.name} alt="pic" src={props.image} onClick={() => props.handleClick(props.id)}/>
         </div>
     </div>   
 
